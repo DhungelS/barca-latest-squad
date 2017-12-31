04752329aee67d07932fca1a951c14567c77f58c
DhungelS <32339127+DhungelS@users.noreply.github.com>
2017-12-31 00:18:35 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require("express");
+var app = express();
+var request = require("request");
+
+
+app.set("view engine", "ejs"); 
+
+
+app.get("/", (req, res) => {
+  
+  var url = "http://api.football-data.org/v1/teams/81/players"
+  request(url, function(error, response, body){
+    if(!error && response.statusCode == 200) {
+        var data = JSON.parse(body)
+        res.render("results", {data: data});
+    }
+ });
+
+})
+
+const port = 5000;
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
